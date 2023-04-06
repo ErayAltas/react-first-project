d11aa970dd028b422bc2283f96318e8b836e9c0e
ErayAltas <altas.erayy@gmail.com>
2023-04-07 00:50:30 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -1,35 +1,33 @@
-import { useEffect, useRef, useState } from 'react'
-import Name from './components/Name'
+import React, { useEffect, useRef, useState } from "react";
+import Name from "./components/Name";
+import { AiFillAlert } from "react-icons/ai";
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [color, setColor] = useState("red")
-  const data = "abc"
+  const [count, setCount] = useState(0);
+  const [color, setColor] = useState("red");
+  const data = "abc";
   const clickFunc = () => {
-    setCount(count+1)
-    if(color === "red"){
-      setColor("blue")
-    }else{
-      setColor("red")
-    } 
-  }
-  
-  const firstRef = useRef(0)
-  console.log(firstRef)
+    setCount(count + 1);
+    if (color === "red") {
+      setColor("blue");
+    } else {
+      setColor("red");
+    }
+  };
+
+  const firstRef = useRef(0);
+  console.log(firstRef);
   useEffect(() => {
-    setColor("purple")
-    firstRef.current.value = 5
-  }, [])
+    setColor("purple");
+  }, []);
 
-
-  
   return (
     <div className="App">
-      <div style={{color: color}}>colorful text</div>
+      <div style={{ color: color }}>colorful text</div>
       {count}
       <Name data={data} />
       <button onClick={clickFunc}>tıkla</button>
-      <input ref={firstRef} placeholder='ara'></input>
+      <AiFillAlert />
     </div>
   );
 }
